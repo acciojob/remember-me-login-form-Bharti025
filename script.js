428a9deccbb97f57db1a3ba2d379b4d0d14fa2bc
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-09-27 14:19:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let user=document.getElementById("username");
 let password=document.getElementById("password");
 let checkbox=document.getElementById("checkbox");
 const loginbtn=document.getElementById("existing");
-loginbtn.style.display="none";
-let isLoggedIn="false";
+loginbtn.style.display='none';
+let isLoggedIn=false;
 
 window.onload = function() {
     isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
@@ -16,14 +16,15 @@ window.onload = function() {
 };
 
 function check(){
-userValue=user.value;
-passwordValue=password.value;
+let userValue=user.value;
+let passwordValue=password.value;
 	if(!checkbox.checked){
 		alert("Logged in as " + userValue);
-		isLoggedIn="false";
+		isLoggedIn=false;
+		localStorage.removeItem("isLoggedIn");
 	}
 	else{
-	isLoggedIn="true";
+	isLoggedIn=true;
      alert("Logged in as " + userValue);
 	 localStorage.setItem("isLoggedIn",isLoggedIn);
     localStorage.setItem("name",userValue);
@@ -38,6 +39,8 @@ loginbtn.style.display='block';
 function exist(){
 	 const userValue = localStorage.getItem("name");
 	alert("Hello "+userValue);
+	localStorage.removeItem("isLoggedIn");
+	loginbtn.style.display='none';
 }
 
 loginbtn.addEventListener("click",exist);
